dfddcd661e998b6d503aefe4b70fd73b68745974
pratik shukla <pratik.shukla1@ibm.com>
2024-08-23 08:15:26 +0530

    changed msg

--- app.py
+++ app.py
@@ -30,8 +30,9 @@ def scrape():
 @app.route("/", methods=['GET'])
 @cross_origin()
 def say_hello():
-    return "hello there, Discovery"
+    return "hello there, Web Scrapper"
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=9001)
+    # app.run(debug=True, host='0.0.0.0', port=9001)
+    app.run(debug=True, host='localhost', port=9001)
